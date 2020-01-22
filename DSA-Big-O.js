4d3a286754d93d6c602b55160d32ae89481ebc54
stephenschab <sschab689@gmail.com>
2020-01-22 09:35:35 -0700

    answered question 2

--- DSA-Big-O.js
+++ DSA-Big-O.js
@@ -2,11 +2,16 @@
 
 1) a)This solution would have a Big O notation of O(1).
     You only need to ask the question once (operation)
-    in order to receive the wanted output (person with a retriever).
+    in order to receive the wanted output (person with
+    a retriever).
 
    b)This solution would hav a Big O notation of O(n).
-   You would use a for loop to iterate through the length of the
-   'arr' of dog owners until you found the dog you are looking for 
-   or ran out of dog owners.
+    You would use a for loop to iterate through the length
+    of the 'arr' of dog owners until you found the dog you
+    are looking for or ran out of dog owners.
+
+2)This solution wuold have a Big O notation of O(1) because
+ there will only ever be one operation running each time
+ you call the function.
 
 */
