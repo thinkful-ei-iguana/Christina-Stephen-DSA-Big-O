a17810c235541c73bdd28eb94992b1895dfc6152
Christina Chapman <cmchapman75@gmail.com>
2020-01-22 10:46:35 -0600

    Are you Here?

--- DSA-Big-O.js
+++ DSA-Big-O.js
@@ -1,17 +1,26 @@
 /*
 
-1) a)This solution would have a Big O notation of O(1).
+1) a) This solution would have a Big O notation of O(1).
     You only need to ask the question once (operation)
     in order to receive the wanted output (person with
     a retriever).
 
-   b)This solution would hav a Big O notation of O(n).
+   b) This solution would hav a Big O notation of O(n).
     You would use a for loop to iterate through the length
     of the 'arr' of dog owners until you found the dog you
     are looking for or ran out of dog owners.
 
-2)This solution wuold have a Big O notation of O(1) because
+2) This solution would have a Big O notation of O(1) because
  there will only ever be one operation running each time
  you call the function.
+
+ 3) This solution would have a Big O notation of O(n^2) because
+ there is a set (2) nested loops. The outer loop will run with the 
+ inner loop (n) times. Each time, the inner loop will run (n) times.
+ (If there was ANOTHER nested loop the complexity would then be
+    o(n^3))
+
+ 4)   
+
 
 */
