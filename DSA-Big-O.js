a81a983b09240d9c2ab9cb320e7edf058fc48741
stephenschab <sschab689@gmail.com>
2020-01-22 09:23:04 -0700

    answered question 1 part 1

---
+++ DSA-Big-O.js
@@ -0,0 +1,9 @@
+/*
+
+1) a)This solution would have a Big O notation of O(1).
+    You only need to ask the question once (operation)
+    in order to receive the wanted output (person with a retriever).
+
+   b)
+
+*/
