6bdee7da40489c8325df7afbdd981a27644e4f4d
Christina Chapman <cmchapman75@gmail.com>
2020-01-22 10:27:46 -0600

    Question 1 section 2

--- DSA-Big-O.js
+++ DSA-Big-O.js
@@ -4,6 +4,9 @@
     You only need to ask the question once (operation)
     in order to receive the wanted output (person with a retriever).
 
-   b)
+   b)This solution would hav a Big O notation of O(n).
+   You would use a for loop to iterate through the length of the
+   'arr' of dog owners until you found the dog you are looking for 
+   or ran out of dog owners.
 
 */
